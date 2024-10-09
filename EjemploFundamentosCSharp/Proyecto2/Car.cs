605fa2a1410fa8b196e2510a9a315758d17e5dc9
Jehan Vente <jventequinones@gmail.com>
2024-10-09 16:33:38 -0500

    Se crea la calse car y el formulario

---
+++ EjemploFundamentosCSharp/Proyecto2/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace Proyecto2
+{
+    public class Car
+    {
+        //Atributos del carro
+        string color;
+        int cilidraje;
+        int modelo;
+
+        //Metodo
+        public string stop()
+        {
+            string msj = "El carro esta detenido";
+            return msj;
+        }
+    }
+}
